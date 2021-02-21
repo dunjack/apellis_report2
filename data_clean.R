e4faa34c60415f2afb3aceced36623956d700dab
dunjack <chinkazoid@gmail.com>
2021-02-21 14:19:08 +0000

    Refined cohort selection, Particularly for Filly2
    This is after figuring out who has Cirrus vs. Heidelberg

--- data_clean.R
+++ data_clean.R
@@ -139,22 +139,41 @@ d_meh <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/
 #########################################
 # Apellis dataset
 #########################################
-# Ethnicity
-d_apel_ethnicity <- as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210131_apellis_events_with_scans.csv", header = T, stringsAsFactors = FALSE)) %>%
+# Demography
+d_apel_demo <- as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210131_apellis_events_with_scans.csv", header = T, stringsAsFactors = FALSE)) %>%
   mutate(patkey = as.character(Subject)) %>%
-  mutate(ethnicity = case_when(
+  mutate(ethnicity1 = case_when(
     ASIAN == "1"~ "asian",
     AMINDIAN == "1"~ "indian",
     BLACK == "1"~ "black",
     HAWAIIAN == "1"~ "hawaiian",
     ETHNIC == "Hispanic or Latino" ~ "hispanic or latino",
     WHITE == "1"~ "caucasian",
-    OTHER == "1" ~ "other")) %>%
-  distinct(patkey, ethnicity)  %>%
-  filter(!is.na(ethnicity))
+    OTHER == "1" ~ "other"),
+    gender = case_when(
+      SEX == "Female" ~ "f",
+      SEX == "Male" ~ "m"),
+    dob = dmy_hms(BRTHDAT)) %>%
+  distinct(patkey, ethnicity1, gender, dob)  %>%
+  filter(!is.na(gender))
 
+# VA # For later consideration of low luminescence VA
+d_apel_va <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210131_apellis_events_with_scans.csv", header = T, stringsAsFactors = FALSE)) %>%
+  mutate(patkey = as.character(Subject), 
+         dat_va = dmy(RecordDay),
+         eye = case_when(
+           BCEYE =="O.S." ~ "l",
+           BCEYE =="O.D." ~ "r"), 
+         va = TVAS_bcva,
+         llva = TVAS_llbcva,
+         time = TargetDays,
+         sdb = paste0((str_pad(SID, 8, pad = "0")),".sdb" ),
+         pat = paste0((str_pad(PID, 8, pad = "0")),".pat" )) %>%
+  distinct(patkey, eye, va, llva, dat_va) %>%
+  filter(!is.na(va)) 
 
-d_apel_event <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210131_apellis_events_with_scans.csv", header = T, stringsAsFactors = FALSE)) %>%
+# OCTs 
+d_apel_oct <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210216_fillyOCTs_processing.csv", header = T, stringsAsFactors = FALSE)) %>%
   mutate(patkey = as.character(Subject), 
          dat_app = dmy(RecordDay),
          oct = case_when(SID >= 1 ~ "1",
@@ -163,169 +182,115 @@ d_apel_event <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/a
            BCEYE =="O.S." ~ "l",
            BCEYE =="O.D." ~ "r"), 
          va = TVAS_bcva,
-         time = TargetDays,
-         dob = dmy_hms(BRTHDAT),
+         time = TargetDay,
          sdb = paste0((str_pad(SID, 8, pad = "0")),".sdb" ),
-         pat = paste0((str_pad(PID, 8, pad = "0")),".pat" )) %>%
-  mutate(dat_app = date(dat_app),
-         gender = case_when(
-           SEX == "Female" ~ "f",
-           SEX == "Male" ~ "m")) %>%
-  select(49:59) %>%
+         pat = paste0((str_pad(PID, 8, pad = "0")),".pat" ))
+
+
+d_apel_fellow_pathology <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210216_filly_felloweyepathology.csv", header = T, stringsAsFactors = FALSE)) %>%
+  mutate(ga = gsub("POT-CP121614-", "", GA.),
+         cnv = gsub("POT-CP121614-", "", CNV.),
+         Study.Fellow = "F") 
+
+
+d_apel_base_fellow <- d_apel_oct %>%
+  filter(time == "-14") %>% # baseline 
+  filter(segmentation_size == "(47, 512)"
+         | segmentation_size == "(49, 512)") %>%
+  semi_join(d_apel_oct %>%  filter(Study.Fellow =="S"), by = "patkey") %>%
+  semi_join(d_apel_fellow_pathology, by = c("Study.Fellow", "patkey" = "ga")) %>%
+  anti_join(d_apel_fellow_pathology, by = c("Study.Fellow", "patkey" = "cnv"))
+
+d_apel_base_study <- d_apel_oct %>%
+  filter(time == "-14") %>% # baseline 
+  filter(segmentation_size == "(47, 512)"
+         | segmentation_size == "(49, 512)") %>%
+  filter(Study.Fellow =="S")
+
+
+d_apel_base <- d_apel_base_study %>%
+  rbind(d_apel_base_fellow) %>%
   mutate(cohort = "filly2") %>%
-  left_join(d_apel_ethnicity, by = "patkey")
-
-
-# VA
-d_apel_va <- d_apel_event %>%
-  filter(!is.na(va)) %>%
-  mutate(dat_va = dat_app) %>%
-  select(patkey, dat_va, eye, va)
-
-# OCT
-d_apel_oct <- d_apel_event %>%
-  filter(oct == "1") %>%
-  mutate(dat_scan = dat_app)  %>%
-  select(patkey, eye, dat_scan, pat, sdb)  
-  
-# Apellis baseline
-d_apel_base <- d_apel_event %>%
-  filter(
-    Study.Fellow == "S"
-         &
-      time <= 0) %>%
-  select(-va, -sdb, -pat) %>%
+  left_join(d_apel_demo, by = "patkey") %>%
   mutate(age = interval(dob, dat_app) / years(1)) %>% # this is baseline age
-  select(-dob) %>%
-  # integrate OCT & VA
-  # left_join(d_apel_oct, by = c("patkey", "eye")) %>%
-  # filter(abs(dat_app - dat_scan) < 30) %>%
-  left_join(d_apel_va, by = c("patkey", "eye")) %>%  
-  filter(abs(dat_app - dat_va) < 30) %>%
+  select(-dob, -va) %>%
+  left_join(d_apel_va, by = c("patkey", "eye")) %>% # Match on VA within 14 days
   group_by(patkey, eye) %>%
-  slice(which.min(time)) %>%
+  filter(abs(dat_va - dat_app) < 14) %>%
+  slice(which.min(abs(dat_va - dat_app))) %>%
   ungroup() %>%
-  mutate(pathology = "ga")
-
-
-fellow <- d_apel_event %>%
-  filter(cohort == "filly2"
-         & Study.Fellow =="S") %>%
-  mutate(
-    Study.Fellow = "F",
-    eye = case_when(
-      eye =="r" ~ "l",
-      eye =="l" ~ "r"), 
-    status = "missing",
-    fovea = "no_annotation") %>% 
-  distinct(patkey, eye ,.keep_all = T) %>%
-  select(-va , -pat, -sdb,-time)
+  select(patkey, eye, pat, sdb, va, llva, gender, ethnicity1, age, cohort, filepath, 13:16)
 
   
 #########################################
 # DATAFRAME Merge
 #########################################
-# All ethnicity
-all_ethnicity <- dimpat %>%
-  select(patkey, ethnicity) %>%
-  rbind(d_apel_ethnicity)
-
 #
-df_all <-  d_meh %>%
-  mutate(dat_app = ymd(app_dat),
-         dat_scan = ymd(dat_scan),
-         time = 0) %>%
-  select(-postcode, -dob, -app_dat, -dat_scan, -sdb, -pat) %>%
-  rbind((d_apel_base %>%
-           select(-oct, -dat_va)))
+df <-  d_apel_base %>%
+  bind_rows(
+    (d_meh %>%
+       mutate(va = as.numeric(va)) %>%
+       select(patkey, eye, pat, sdb, va, gender, ethnicity, age, cohort) ))
+
+
   
 
 # What Sophie has so far
-df <- as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210204_df_complete_v1.csv", header = T, stringsAsFactors = FALSE))  %>%
-  mutate(
-    patkey = as.character(patkey),
-    dat_app = ymd(dat_app),
-    dat_scan = ymd(dat_scan),
-    sdb = paste0((str_pad(sdb, 8, pad = "0")),".sdb" ),
-    pat = paste0((str_pad(pat, 8, pad = "0")),".pat" )) %>%
-  select(-X) %>%
-  # filter(!Study.Fellow == "F") %>%
-  # Adding on OCTs that are missing but should be there
-  # bind_rows is basically rbind.fill()
-  bind_rows(( df_all %>%
-                anti_join(df, by = c("patkey", "eye"))%>%
-                mutate(va = as.numeric(va)))) %>%
-  select(-ethnicity) %>%
-  left_join(all_ethnicity, by = "patkey") 
-
-
-
-######
-
-##### All FILLY scans so far #######
-d_filly_v1 <- as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210131_apellis_events_with_scans.csv", header = T, stringsAsFactors = FALSE)) %>%
-  mutate(patkey = as.character(Subject), 
-         dat_app = dmy(RecordDay),
-         oct = case_when(SID >= 1 ~ "1",
-                         T ~ "0"),
-         eye = case_when(
-           BCEYE =="O.S." ~ "l",
-           BCEYE =="O.D." ~ "r"), 
-         va = TVAS_bcva,
-         time = TargetDays,
-         dob = dmy_hms(BRTHDAT),
-         sdb = paste0((str_pad(SID, 8, pad = "0")),".sdb" ),
-         pat = paste0((str_pad(PID, 8, pad = "0")),".pat" )) %>%
-  mutate(dat_app = date(dat_app),
-         gender = case_when(
-           SEX == "Female" ~ "f",
-           SEX == "Male" ~ "m")) %>%
-  select(49:59) %>%
-  filter(Study.Fellow == "S") %>%
-  distinct(patkey,.keep_all = T)
-  
-
-d_filly_v2 <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210216_fillyOCTs_processing.csv", header = T, stringsAsFactors = FALSE))  %>%
-  mutate(
-    patkey = as.character(Subject),
-    dat_app = dmy(RecordDay),
-    dat_scan = dmy(RecordDay),
-    sdb = paste0((str_pad(SID, 8, pad = "0")),".sdb" ),
-    pat = paste0((str_pad(PID, 8, pad = "0")),".pat" )) %>%
-  select(-X) %>%
-  filter(Study.Fellow == "S") %>%
-  distinct(PID,.keep_all = T)
-
-
-
-
-write.csv(filly, file="210217_filly2_annotation2.csv")
-
-
-
-
-
-
-
-  semi_join(df_all, by = "pat")
-
-x <- df_all %>%
-  filter(cohort == "filly2") %>%
-  anti_join(filly, by = c("patkey", "Study.Fellow"))
-
-
-x1 <- filly %>%
-  anti_join(df_all, by = c("patkey", "Study.Fellow"))
-# %>%
-#   bind_rows(x) %>%
+# df <- as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210204_df_complete_v1.csv", header = T, stringsAsFactors = FALSE))  %>%
 #   mutate(
-#     fovea = case_when(
-#       status =="interpolated" ~ "annotated_interpolated",
-#       T ~ "no_annotation"),
-#     status = case_when(
-#       status =="interpolated" ~ "segmented",
-#       T ~ "missing")
-#   )
+#     patkey = as.character(patkey),
+#     dat_app = ymd(dat_app),
+#     dat_scan = ymd(dat_scan),
+#     sdb = paste0((str_pad(sdb, 8, pad = "0")),".sdb" ),
+#     pat = paste0((str_pad(pat, 8, pad = "0")),".pat" )) %>%
+#   select(-X) %>%
+#   # filter(!Study.Fellow == "F") %>%
+#   # Adding on OCTs that are missing but should be there
+#   # bind_rows is basically rbind.fill()
+#   bind_rows(( df_all %>%
+#                 anti_join(df, by = c("patkey", "eye")) %>%
+#                 mutate(va = as.numeric(va)))) %>%
+#   select(-ethnicity) %>%
+#   left_join(all_ethnicity, by = "patkey") 
 # 
 # 
-# write.csv(df, file = "210208_dataframe.csv")
+# 
+# ######
+# 
+# ##### All FILLY scans so far #######
+# d_filly_v1 <- as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210131_apellis_events_with_scans.csv", header = T, stringsAsFactors = FALSE)) %>%
+#   mutate(patkey = as.character(Subject), 
+#          dat_app = dmy(RecordDay),
+#          oct = case_when(SID >= 1 ~ "1",
+#                          T ~ "0"),
+#          eye = case_when(
+#            BCEYE =="O.S." ~ "l",
+#            BCEYE =="O.D." ~ "r"), 
+#          va = TVAS_bcva,
+#          time = TargetDays,
+#          dob = dmy_hms(BRTHDAT),
+#          sdb = paste0((str_pad(SID, 8, pad = "0")),".sdb" ),
+#          pat = paste0((str_pad(PID, 8, pad = "0")),".pat" )) %>%
+#   mutate(dat_app = date(dat_app),
+#          gender = case_when(
+#            SEX == "Female" ~ "f",
+#            SEX == "Male" ~ "m")) %>%
+#   select(49:59) %>%
+#   filter(Study.Fellow == "S") %>%
+#   distinct(patkey,.keep_all = T)
+#   
+# 
+# d_filly_v2 <-  as.data.frame(read.csv("/Users/Dunistrator/Documents/MEH_data/apellis/trial_cohort/210216_fillyOCTs_processing.csv", header = T, stringsAsFactors = FALSE))  %>%
+#   mutate(
+#     patkey = as.character(Subject),
+#     dat_app = dmy(RecordDay),
+#     dat_scan = dmy(RecordDay),
+#     sdb = paste0((str_pad(SID, 8, pad = "0")),".sdb" ),
+#     pat = paste0((str_pad(PID, 8, pad = "0")),".pat" )) %>%
+#   select(-X) %>%
+#   filter(Study.Fellow == "S") %>%
+#   distinct(PID,.keep_all = T)
+
+
+
+# 
